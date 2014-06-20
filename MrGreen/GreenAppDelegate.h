9269f2bbe4f798baf0f8f33855628986430af2ba
Michael Zhao <mizhao@mizhao-mn1.linkedin.biz>
2014-06-20 11:09:36 -0700

    Initial Commit

---
+++ MrGreen/GreenAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GreenAppDelegate.h
+//  MrGreen
+//
+//  Created by Michael Zhao on 6/20/14.
+//  Copyright (c) 2014 Michael Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GreenAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
